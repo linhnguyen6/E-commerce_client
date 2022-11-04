9097315719e4946288d00cad8f5aa76211e9419f
Nguyen Linh <linh.nguyen6@tda.company>
2022-11-04 09:06:21 +0700

    updated

--- src/pages/Checkout/Payment.js
+++ src/pages/Checkout/Payment.js
@@ -24,7 +24,7 @@ const Payment = () => {
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: process.env.REACT_APP_CART_PAGE,
+        return_url: "https://tiki.pages.dev/payment/cart",
       },
     });
 
